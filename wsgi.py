e2de1cb1b52288f5a044cf5d4cac43c252c1b19f
liao-l-x <1530837611@qq.com>
2018-10-27 13:03:59 +0800

    edit 1 files

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+第一次改的地方
 """
 
 import os
